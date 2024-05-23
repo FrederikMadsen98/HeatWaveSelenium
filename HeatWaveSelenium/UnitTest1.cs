5b18eea85b08df8e3be66bfcbdb02f731dbd79fa
Frederik Madsen <fmadsen1.10.98@gmail.com>
2024-05-23 10:13:49 +0200

    Kommentarer er tilføjet

--- HeatWaveSelenium/UnitTest1.cs
+++ HeatWaveSelenium/UnitTest1.cs
@@ -12,8 +12,13 @@ namespace HeatwaveSelenium
     public class UnitTest1
 
     {
+        //URL for hjemmesiden som skal testes
         private static readonly string _baseUrl = "https://happy-water-0902f6503.5.azurestaticapps.net/";
+
+        //Driveren som skal bruges til at interagere med hjemmesiden
         private static IWebDriver _driver;
+
+        //Initialisering testklassen og opretter en instans af driveren
         [ClassInitialize]
         public static void Setup(TestContext context)
         {
@@ -21,31 +26,45 @@ namespace HeatwaveSelenium
             _driver.Navigate().GoToUrl(_baseUrl);
 
         }
+
+        //Lukker driveren efter testen er kørt
         [ClassCleanup]
         public static void TearDown()
         {
             _driver.Dispose();
         }
 
+        //Tester om at tabellen vises efter at have trykket på knappen
         [TestMethod]
         public void TestMethod1()
         {
+            //Navigerer til hjemmesiden
             _driver.Navigate().GoToUrl("https://happy-water-0902f6503.5.azurestaticapps.net/");
+
+            //Finder knappen med id "2" og trykker på den
             IWebElement buttonshowlist = _driver.FindElement(By.Id("2"));
             buttonshowlist.Click();
+
+            //Finder tabellen med id "myTable" og tjekker om den er vist
             IWebElement table = _driver.FindElement(By.Id("myTable"));
             Assert.IsTrue(table.Displayed);
         }
+
+        //Tester om knappen "Full" kan klikkes på
         [TestMethod]
         public void TestMethod2()
         {
+            //Finder og trykker på knappen med id "full"
             IWebElement buttonshow = _driver.FindElement(By.Id("full"));
             buttonshow.Click();
 
         }
+
+        //Tester om slideren kan bruges
         [TestMethod]
         public void TestMethod3()
         {
+            //Finder slideren og bruger Keys.ArrowRight til at flytte den
             WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
             IWebElement slider = wait.Until(driver => driver.FindElement(By.Id("myRange")));
 
